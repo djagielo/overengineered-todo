4a89c8dcda6c0dd3c7604ab5329075d0219f426f
djagielo <dariusz.jagielo@gmail.com>
2020-05-09 22:26:10 +0200

    [master] Ambicion service - initial commit

---
+++ src/main/java/me/djagielo/ambitionservice/AmbitionServiceApplication.java
@@ -0,0 +1,13 @@
+package me.djagielo.ambitionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbitionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbitionServiceApplication.class, args);
+	}
+
+}
