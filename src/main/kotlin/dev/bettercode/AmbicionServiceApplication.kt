e5ab002ac35ea584e563b7afa9cac91a4ea4cb60
djagielo <dariusz.jagielo@gmail.com>
2021-02-14 18:27:28 +0100

    preparing structure of the project

---
+++ src/main/kotlin/dev/bettercode/AmbicionServiceApplication.kt
@@ -0,0 +1,11 @@
+package dev.bettercode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AmbicionServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AmbicionServiceApplication>(*args)
+}
