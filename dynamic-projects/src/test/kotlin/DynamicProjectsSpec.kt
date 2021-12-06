c2971d344f62d602419b0c2a25586c953c3e7813
djagielo <dariusz.jagielo@gmail.com>
2021-12-06 08:45:23 +0100

    Added test-case reproducing the issue with tasks not being removed from dynamic project after they no longer meet the conditions of it

--- dynamic-projects/src/test/kotlin/DynamicProjectsSpec.kt
+++ dynamic-projects/src/test/kotlin/DynamicProjectsSpec.kt
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 class DynamicProjectsSpec {
 
@@ -30,7 +31,7 @@ class DynamicProjectsSpec {
     private val tasksFacade: TasksFacade = mock(TasksFacade::class.java)
 
     private val dynamicProjectHandlers: DynamicProjectHandlers =
-        DynamicProjectsConfiguration.dynamicProjectHandlers(tasksFacade, )
+        DynamicProjectsConfiguration.dynamicProjectHandlers(tasksFacade)
 
     @Test
     fun `should create predefined dynamic-projects after first project gets created`() {
@@ -42,7 +43,7 @@ class DynamicProjectsSpec {
 
         // then
         assertThat(dynamicProjects.map { it.name }).containsExactlyInAnyOrder(
-            "Completed today", "Today", "Overdue", "Completed last week"
+            "Completed today", "Today", "Overdue"
         )
     }
 
@@ -57,9 +58,9 @@ class DynamicProjectsSpec {
 
         // and - list of tasks
         val tasks = listOf(
-            TaskDto(name = "task_due_yesterday", dueDate = LocalDate.now().plusDays(1)),
-            TaskDto(name = "task_due_2_days_ago", dueDate = LocalDate.now().plusDays(2)),
-            TaskDto(name = "task_due_month_ago", dueDate = LocalDate.now().plusDays(30))
+            TaskDto(name = "task_due_tomorrow", dueDate = LocalDate.now().plusDays(1)),
+            TaskDto(name = "task_due_in_2_days", dueDate = LocalDate.now().plusDays(2)),
+            TaskDto(name = "task_due_in_a_month", dueDate = LocalDate.now().plusDays(30))
         )
 
         val tasksOverdue = listOf(
@@ -77,5 +78,55 @@ class DynamicProjectsSpec {
         assertThat(dynamicProjectsFacade.getProjectById(overdueProject.id)!!.tasks).hasSameElementsAs(
             tasksOverdue.map { it.id }
         )
+    }
+
+    @Test
+    fun `should recalculate Overdue dynamic project membership when tasks no longer meet conditions`() {
+        // given - any project to generate default ones
+        dynamicProjectHandlers.handleProjectCreated(ProjectCreated(ProjectId()))
+        // and - Overdue project created as a result
+        val overdueProject = dynamicProjectsFacade.getProjectByName("Overdue")
+        assertThat(overdueProject).isNotNull
+        overdueProject!!
+
+        // and - list of tasks
+        val tasks = listOf(
+            TaskDto(name = "task_due_tomorrow", dueDate = LocalDate.now().plusDays(1)),
+            TaskDto(name = "task_due_in_2_days", dueDate = LocalDate.now().plusDays(2)),
+            TaskDto(name = "task_due_in_a_month", dueDate = LocalDate.now().plusDays(30))
+        )
+
+        var tasksOverdue = listOf(
+            TaskDto(name = "task_due_yesterday", dueDate = LocalDate.now().minusDays(1)),
+            TaskDto(name = "task_due_2_days_ago", dueDate = LocalDate.now().minusDays(2)),
+            TaskDto(name = "task_due_month_ago", dueDate = LocalDate.now().minusDays(30))
+        )
+        doReturn(Page.empty<TaskDto>()).`when`(tasksFacade).getAllOpen(notEq(PageRequest.of(0, 100)))
+        doReturn(PageImpl((tasks + tasksOverdue).shuffled())).`when`(tasksFacade).getAllOpen(eq(PageRequest.of(0, 100)))
+
+
+        dynamicProjectHandlers.recalculateProject(RecalculateProject(overdueProject.id))
+
+        // and tasks are added to the dynamic project
+        assertThat(dynamicProjectsFacade.getProjectById(overdueProject.id)!!.tasks).hasSameElementsAs(
+            tasksOverdue.map { it.id }
+        )
+
+        // when - tasks have changed
+        tasksOverdue = tasksOverdue.map {
+            TaskDto(
+                id = it.id,
+                name = it.name,
+                completionDate = it.completionDate,
+                dueDate = LocalDate.now().plus(1, ChronoUnit.MONTHS)
+            )
+        }
+        doReturn(PageImpl((tasks + tasksOverdue).shuffled())).`when`(tasksFacade).getAllOpen(eq(PageRequest.of(0, 100)))
+
+        // and - project gets recalculated again
+        dynamicProjectHandlers.recalculateProject(RecalculateProject(overdueProject.id))
+
+        // then
+        assertThat(dynamicProjectsFacade.getProjectById(overdueProject.id)!!.tasks).isEmpty()
     }
 }
